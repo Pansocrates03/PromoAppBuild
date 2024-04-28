cd2fe841628d5ed651caca2fcd0f77221baf5ce4
Esteban Sierra Baccio <87460697+Pansocrates03@users.noreply.github.com>
2024-04-27 18:36:50 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+// import LandingPage from "./LandingPage";
+import MainApp from "./MainApp";
+
+const App = () => {
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // const handleLogin = () => {
+  //   setIsLoggedIn(true);
+  // };
+
+  // const handleLogout = () => {
+  //   setIsLoggedIn(false);
+  // };
+
+  return (
+
+      <MainApp />
+  );
+};
+
+export default App;
